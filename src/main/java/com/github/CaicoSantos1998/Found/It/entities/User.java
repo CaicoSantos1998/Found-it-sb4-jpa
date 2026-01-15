18c3a763cadaf9f45d290e0a2be55cad87a3a8fc
CS <caico_fc@hotmail.com>
2026-01-14 23:42:36 -0300

    H2 database, test profile and JPA

--- src/main/java/com/github/CaicoSantos1998/Found/It/entities/User.java
+++ src/main/java/com/github/CaicoSantos1998/Found/It/entities/User.java
@@ -1,21 +1,25 @@
 package com.github.CaicoSantos1998.Found.It.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
-
     public User() {
     }
 
