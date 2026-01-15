84da28983e2deb57ab3f42dc9c3dd6bcb080463c
CS <caico_fc@hotmail.com>
2026-01-15 13:01:02 -0300

    Service layer, component registration

--- src/main/java/com/github/CaicoSantos1998/Found/It/config/TestConfig.java
+++ src/main/java/com/github/CaicoSantos1998/Found/It/config/TestConfig.java
@@ -17,7 +17,7 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User u1 = ncew User(null, "Maria Brown", "maria@gmail.com", "98837421", "12452");
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98837421", "12452");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "98873221", "432222");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
---
+++ src/main/java/com/github/CaicoSantos1998/Found/It/services/UserService.java
@@ -0,0 +1,24 @@
+package com.github.CaicoSantos1998.Found.It.services;
+
+import com.github.CaicoSantos1998.Found.It.entities.User;
+import com.github.CaicoSantos1998.Found.It.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> optional = repository.findById(id);
+        return optional.get();
+    }
+}
